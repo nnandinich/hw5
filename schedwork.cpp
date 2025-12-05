c74a3f71dd77b12b1b04945883a46cf04a8ba479
Nandini Chennupaty <nchennup@usc.edu>
2025-12-05 01:09:26 +0000

    finished with wordle and schedule

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    size_t slot,
+    vector<size_t>& workerCurrShift
+    );
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +46,54 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    sched.resize(avail.size());
+    for(size_t i=0; i<avail.size(); i++){
+      sched[i].resize(dailyNeed, INVALID_ID);
+    }
+    vector<size_t> workerCurrShift(avail[0].size(), 0);
+    bool result= scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, workerCurrShift);
+    return result;
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    size_t slot,
+    vector<size_t>& workerCurrShift
+    )
+ {   
+  //number of days
+  size_t n=avail.size();
+  size_t totalWorkers=avail[0].size();
+  //if all days scheduled
+  if(day==n){
+    return true;
+  }
+  //if day slots are filled, go to next day
+  if(slot==dailyNeed){
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, day+1, 0, workerCurrShift);
+  }
+  //find workers for slot
+  for(size_t i=0; i<totalWorkers; i++ ){
+    //need to make sure worker isnt scheduled on this day
+    if(find(sched[day].begin(), sched[day].end(), i)==sched[day].end()){
+      //current worker is avaiable on this day and see if over max shifts
+      if(avail[day][i] && workerCurrShift[i]<maxShifts){
+        //assign worker to shift
+        sched[day][slot]=i;
+        workerCurrShift[i]++;
+        //move to next spot
+        if(scheduleHelper(avail, dailyNeed, maxShifts, sched, day, slot+1, workerCurrShift)){
+          return true;
+        } 
+        //if it doesnt work, go back
+        sched[day][slot]=INVALID_ID;
+        workerCurrShift[i]-=1;
+        }
+      }
+    }
+    //no worker can work for this slot
+      return false;
+ }
--- wordle.cpp
+++ wordle.cpp
@@ -13,8 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
-
+void wordleHelper(std::string& curr, int index, const std::string& in, int floatingLeft, const std::set<std::string>& dict, std::set<std::string>& output, int floatingMultiples[26]);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +21,68 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> output;
+    string curr=in;
+    //create array of each letter and how many floating of each there are
+    int floatingMultiples[26]={0};
+    //how many floating letters are left
+    int floatingLeft=0;
+    for(size_t i=0; i<floating.size(); i++){
+        floatingMultiples[floating[i]-'a']++;
+        floatingLeft++;
+    }
+    wordleHelper(curr, 0, in, floatingLeft, dict, output, floatingMultiples);
+    return output;
+    
 }
 
 // Define any helper functions here
+void wordleHelper(std::string& curr, int index, const std::string& in, int floatingLeft, const std::set<std::string>& dict, std::set<std::string>& output, int floatingMultiples[26]){
+  //index is pos about to fill
+  //in is dashes and letters
+  //floatingLeft is remaining floating
+  //floatingMultiples is the number of floating of each letter
+  //output is all the possible words that it could be
+  
+  //full word has been made
+  if(index==int(in.size())){
+    if( floatingLeft==0 && dict.find(curr)!=dict.end()){
+      output.insert(curr);
+    }
+    return;
+  }
+  //pruning, more floating than blank spaces
+  if(floatingLeft > (int(in.size()) - index)){
+    return;
+  }  
+  
+  //found green letter
+  if(in[index] != '-'){
+    //nothing to do here, just move to the next index
+    wordleHelper(curr,index+1,in,floatingLeft,dict, output, floatingMultiples);
+  }  
+  //found dash, need to be filled in
+  else if(in[index]=='-'){ 
+    //go through all the letters
+    for (char letter='a'; letter<='z'; letter++){
+      //test current letter
+      curr[index]=letter;
+      //try to use floating
+      floatingMultiples[letter-'a']--;
+      int x =floatingMultiples[letter-'a'];
+      //if floating can be used decrease floating left
+      if(x>=0){
+        floatingLeft--;
+      }
+      //recure to next index with this 
+      wordleHelper(curr,index+1,in,floatingLeft,dict, output, floatingMultiples);
+      //if it didnt work, need to backtrack
+      if(x>=0){
+        floatingLeft++;
+      }
+      floatingMultiples[letter-'a']++;
+      curr[index]='-';
+    }
+  }
+}  
+      
